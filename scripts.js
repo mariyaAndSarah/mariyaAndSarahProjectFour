e901bb76a9fa691d87528e86bde81d6b6cc9a6cf
sarahmgans <sarah.gans@hotmail.com>
2020-04-13 21:22:02 -0400

    added some scripts

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-// scripts go here
+// Create a namespace for the app
+const myElephants = {};
+
+// Create function that will make ajax request to be called inside of the init function
+myElephants.getElephants = () => {
+    $.ajax({
+        url: `https://elephant-api.herokuapp.com/elephants/random`,
+        method: 'GET',
+        dataType: 'jsonp'
+    }).then(result => {
+        console.log(result);
+    })
+}
+
+// Create an init function for the app
+myElephants.init = () => {
+    myElephants.getElephants();
+};
+
+// Create a document ready function for the app 
+$(function(){
+    myElephants.init();
+});
